c569d70179534accb3a178094fbf14d67ab4cab6
ishitha08 <58905663+ishitha08@users.noreply.github.com>
2020-08-03 18:58:27 +0530

    Add files via upload

---
+++ c110a.py
@@ -0,0 +1,14 @@
+import plotly.figure_factory as ff
+import statistics
+import random
+import pandas as pd
+import csv
+
+df = pd.read_csv('data.csv')
+data = df['average'].tolist()
+population_mean = statistics.mean(data)
+
+fig = ff.create_distplot([data],['average'],show_hist = False)
+fig.show()
+
+
